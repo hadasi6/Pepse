a0dfe21ccf162383533cfef5348fda840c708377
Owner-Hadas <hadas.elezra@mail.huji.ac.il>
2024-12-29 01:41:40 +0200

    initial commit

---
+++ src/pepse/PepseGameManager.java
@@ -0,0 +1,26 @@
+package pepse;
+
+import danogl.GameManager;
+import danogl.GameObject;
+import danogl.collisions.Layer;
+import danogl.gui.ImageReader;
+import danogl.gui.SoundReader;
+import danogl.gui.UserInputListener;
+import danogl.gui.WindowController;
+import pepse.world.Sky;
+
+public class PepseGameManager extends GameManager {
+    public static void main(String[] args) {
+        new PepseGameManager().run();
+    }
+
+    @Override
+    public void initializeGame(ImageReader imageReader,
+                               SoundReader soundReader, UserInputListener inputListener,
+                               WindowController windowController) {
+        GameObject sky = Sky.create(windowController.getWindowDimensions());
+        gameObjects().addGameObject(sky, Layer.BACKGROUND); //
+    }
+
+
+}
